15e4518616dadb0cad14341b73a4002b41ef678a
sophie202205 <sophie6995137@gmail.com>
2022-05-15 21:32:55 +0800

    安排日程做的事顯示在TextView 影片43

--- app/src/main/java/com/example/timertest/MainActivity.java
+++ app/src/main/java/com/example/timertest/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.timertest;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -12,10 +15,14 @@ public class MainActivity extends AppCompatActivity {
 
     private Timer timer;
     private Task1 task1;
+    private TextView msg;
+    private int counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        msg = findViewById(R.id.msg);
     }
 
     @Override
@@ -43,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
     public void test2(View view) {
         //(要做的事情,延遲幾秒,間隔多久再做一次)
         task1 = new Task1();
-        timer.schedule(task1, 1*1000, 3*1000);
+        timer.schedule(task1, 1*1000, 1*1000);
     }
 
     public void test3(View view) {
@@ -53,10 +60,17 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void test4(View view) {
+        Intent intent = new Intent(this, Page2Activity.class);
+        startActivity(intent);
+    }
+
     public class Task1 extends TimerTask{
         @Override
         public void run() {
-            Log.v("brad", "OK");
+            //Log.v("brad", "OK");
+            counter++;
+            msg.setText("" + counter); //直接設counter會是設定resource的ID
         }
     }
 }
