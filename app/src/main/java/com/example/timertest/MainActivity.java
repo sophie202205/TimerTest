bdcb47b8c6567f741a49f214523c114089fee9c2
sophie202205 <sophie6995137@gmail.com>
2022-05-15 21:03:04 +0800

    new TimerTask class 安排日程 影片40

--- app/src/main/java/com/example/timertest/MainActivity.java
+++ app/src/main/java/com/example/timertest/MainActivity.java
@@ -2,16 +2,47 @@ package com.example.timertest;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Timer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        timer = new Timer();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(timer != null){
+            timer.cancel(); //取消
+            timer.purge(); //清除
+            timer = null;
+        }
+    }
+
     public void test1(View view) {
+        Log.v("brad", "start");
+        //千分之一秒
+        timer.schedule(new Test1(), 3*1000); //安排之日程
+    }
+
+    public class Test1 extends TimerTask{
+        @Override
+        public void run() {
+            Log.v("brad", "OK");
+        }
     }
 }
